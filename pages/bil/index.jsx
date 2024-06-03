e67bbde11daa66a3ffa1ff38053b092dabd5b92d
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-06-03 18:18:00 +0700

    Update index.jsx

--- pages/bil/index.jsx
+++ pages/bil/index.jsx
@@ -7,7 +7,8 @@ function index() {
        <Head>
         <title>Bil:แจ้งรายละเอียดค่าบริการ</title>
         <meta name="description" content="Bil:แจ้งรายละเอียดค่าบริการโฆษณา และ รายละเอียดต่าง" />
-        
+           <meta name="twitter:image" content="/img/logo.png"/>
+    <meta property="og:image" content="/img/logo.png"/>  
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://mybil.adsdep.com/" />
         <meta property="og:title" content="Bil:แจ้งรายละเอียดค่าบริการ" />
